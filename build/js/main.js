00acfad0a1d676705188901fc945b7c2b5e719c2
Vladimir <fayst_@bk.ru>
2020-11-12 17:05:01 +0300

    Устранил часть замечаний по проекту.

--- build/js/main.js
+++ build/js/main.js
@@ -8,163 +8,172 @@ function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _d
 
 function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
-var Slider = /*#__PURE__*/function () {
-  function Slider(slider) {
-    var _this = this;
+(function () {
+  var Slider = /*#__PURE__*/function () {
+    function Slider(slider) {
+      var _this = this;
 
-    _classCallCheck(this, Slider);
+      _classCallCheck(this, Slider);
 
-    _defineProperty(this, "sliderLeft", function () {
-      _this.viewSlide += _this.sliderViewport + _this.sliderMarginRight;
+      _defineProperty(this, "sliderLeft", function () {
+        _this.viewSlide += _this.sliderViewport + _this.sliderMarginRight;
 
-      if (_this.viewSlide > _this.sliders.offsetWidth - (_this.sliderViewport + _this.sliderMarginRight)) {
-        _this.viewSlide = 0;
-      } else {
-        _this.viewSlid = _this.sliders.offsetWidth - (_this.sliderViewport + _this.sliderMarginRight);
-      }
+        if (_this.viewSlide > _this.sliders.offsetWidth - (_this.sliderViewport + _this.sliderMarginRight)) {
+          _this.viewSlide = 0;
+        } else {
+          _this.viewSlid = _this.sliders.offsetWidth - (_this.sliderViewport + _this.sliderMarginRight);
+        }
 
-      _this.sliders.style.right = "".concat(_this.viewSlide, "px");
-    });
-
-    _defineProperty(this, "sliderRigth", function () {
-      _this.viewSlide -= _this.sliderViewport + _this.sliderMarginRight;
-      _this.viewSlide = _this.viewSlide < 0 ? _this.sliders.offsetWidth - (_this.sliderViewport + _this.sliderMarginRight) : _this.viewSlide;
-      _this.sliders.style.right = "".concat(_this.viewSlide, "px");
-    });
-
-    //Управление слайдером
-    this.sliderControl = document.querySelector(".".concat(slider, "__slider_control"));
-    this.sliderBtnNext = this.sliderControl.querySelector(".".concat(slider, "__button_next"));
-    this.sliderBtnBack = this.sliderControl.querySelector(".".concat(slider, "__button_back")); //Слайдер
-
-    this.sliderBlock = document.querySelector(".".concat(slider, "__slider_block"));
-    this.sliderViewport = this.sliderBlock.offsetWidth;
-    this.sliders = this.sliderBlock.querySelector(".".concat(slider, "__sliders"));
-    this.right = parseInt(window.getComputedStyle(this.sliders, null).getPropertyValue("right"));
-    this.slider = this.sliders.querySelector(".".concat(slider, "__slider_item"));
-    this.sliderMarginRight = parseInt(window.getComputedStyle(this.slider, null).getPropertyValue("margin-right"));
-    this.viewSlide = this.right;
-    this.sliderInit();
-  }
-
-  _createClass(Slider, [{
-    key: "sliderInit",
-    value: function sliderInit() {
-      var _this2 = this;
-
-      this.sliderBtnNext.addEventListener('click', function (evt) {
-        evt.preventDefault();
-
-        _this2.sliderLeft();
+        _this.sliders.style.right = "".concat(_this.viewSlide, "px");
       });
-      this.sliderBtnBack.addEventListener('click', function (evt) {
-        evt.preventDefault();
 
-        _this2.sliderRigth();
+      _defineProperty(this, "sliderRight", function () {
+        _this.viewSlide -= _this.sliderViewport + _this.sliderMarginRight;
+        _this.viewSlide = _this.viewSlide < 0 ? _this.sliders.offsetWidth - (_this.sliderViewport + _this.sliderMarginRight) : _this.viewSlide;
+        _this.sliders.style.right = "".concat(_this.viewSlide, "px");
       });
+
+      //Управление слайдером
+      this.sliderControl = document.querySelector(".".concat(slider, "__slider_control"));
+      this.sliderBtnNext = this.sliderControl.querySelector(".".concat(slider, "__button_next"));
+      this.sliderBtnBack = this.sliderControl.querySelector(".".concat(slider, "__button_back")); //Слайдер
+
+      this.sliderBlock = document.querySelector(".".concat(slider, "__slider_block"));
+      this.sliderViewport = this.sliderBlock.offsetWidth;
+      this.sliders = this.sliderBlock.querySelector(".".concat(slider, "__sliders"));
+      this.right = parseInt(window.getComputedStyle(this.sliders, null).getPropertyValue("right"));
+      this.slider = this.sliders.querySelector(".".concat(slider, "__slider_item"));
+      this.sliderMarginRight = parseInt(window.getComputedStyle(this.slider, null).getPropertyValue("margin-right"));
+      this.viewSlide = this.right;
+      this.sliderInit();
     }
-  }]);
 
-  return Slider;
-}();
+    _createClass(Slider, [{
+      key: "sliderInit",
+      value: function sliderInit() {
+        var _this2 = this;
 
-new Slider('coaches');
-new Slider('reviews');
-
-var Scrolling = /*#__PURE__*/function () {
-  function Scrolling() {
-    _classCallCheck(this, Scrolling);
-
-    this.linkNav = document.querySelectorAll('[href^="#"]');
-    this.linkNavArr = Array.prototype.slice.call(this.linkNav);
-    this.speed = 1;
-    this.scrollInit();
-  }
-
-  _createClass(Scrolling, [{
-    key: "scrollInit",
-    value: function scrollInit() {
-      var _this3 = this;
-
-      this.linkNavArr.forEach(function (el) {
-        el.addEventListener('click', function (evt) {
+        this.sliderBtnNext.addEventListener("click", function (evt) {
           evt.preventDefault();
-          var speed = _this3.speed;
-          var w = window.pageYOffset;
-          var hash = evt.target.href.replace(/[^#]*(.*)/, '$1');
-          var t = document.querySelector(hash).getBoundingClientRect().top;
-          var start = null;
-          requestAnimationFrame(step);
 
-          function step(time) {
-            if (start === null) start = time;
-            var progress = time - start;
-            var r = t < 0 ? Math.max(w - progress / speed, w + t) : Math.min(w + progress / speed, w + t);
-            window.scrollTo(0, r);
+          _this2.sliderLeft();
+        });
+        this.sliderBtnBack.addEventListener("click", function (evt) {
+          evt.preventDefault();
 
-            if (r != w + t) {
-              requestAnimationFrame(step);
-            } else {
-              location.hash = hash;
-            }
-          }
-        }, false);
-      });
-    }
-  }]);
+          _this2.sliderRight();
+        });
+      }
+    }]);
 
-  return Scrolling;
-}();
+    return Slider;
+  }();
 
-new Scrolling();
+  new Slider("coaches");
+  new Slider("reviews");
+})();
+
 'use strict';
 
-var Tabs = /*#__PURE__*/function () {
-  function Tabs(controls, tabElements) {
-    _classCallCheck(this, Tabs);
+(function () {
+  var Scrolling = /*#__PURE__*/function () {
+    function Scrolling() {
+      _classCallCheck(this, Scrolling);
 
-    _defineProperty(this, "toggleClassBtn", function (arrBtn, target) {
-      arrBtn.forEach(function (el) {
-        return el.classList.remove('subscription__tab_control--active');
+      this.linkNav = document.querySelectorAll('[href^="#"]');
+      this.linkNavArr = Array.prototype.slice.call(this.linkNav);
+      this.speed = 1;
+      this.scrollInit();
+    }
+
+    _createClass(Scrolling, [{
+      key: "scrollInit",
+      value: function scrollInit() {
+        var _this3 = this;
+
+        this.linkNavArr.forEach(function (el) {
+          el.addEventListener('click', function (evt) {
+            evt.preventDefault();
+            var speed = _this3.speed;
+            var w = window.pageYOffset;
+            var hash = evt.target.href.replace(/[^#]*(.*)/, '$1');
+            var t = document.querySelector(hash).getBoundingClientRect().top;
+            var start = null;
+            requestAnimationFrame(step);
+
+            function step(time) {
+              if (start === null) start = time;
+              var progress = time - start;
+              var r = t < 0 ? Math.max(w - progress / speed, w + t) : Math.min(w + progress / speed, w + t);
+              window.scrollTo(0, r);
+
+              if (r != w + t) {
+                requestAnimationFrame(step);
+              } else {
+                location.hash = hash;
+              }
+            }
+          }, false);
+        });
+      }
+    }]);
+
+    return Scrolling;
+  }();
+
+  new Scrolling();
+})();
+
+'use strict';
+
+(function () {
+  var Tabs = /*#__PURE__*/function () {
+    function Tabs(controls, tabElements) {
+      _classCallCheck(this, Tabs);
+
+      _defineProperty(this, "toggleClassBtn", function (arrBtn, target) {
+        arrBtn.forEach(function (el) {
+          return el.classList.remove("subscription__tab_control--active");
+        });
+        target.classList.add("subscription__tab_control--active");
       });
-      target.classList.add('subscription__tab_control--active');
-    });
 
-    _defineProperty(this, "toggleTab", function (arr, i) {
-      arr.forEach(function (tab, index, array) {
-        tab.classList.remove('subscription__tabs--active');
+      _defineProperty(this, "toggleTab", function (arr, i) {
+        arr.forEach(function (tab, index, array) {
+          tab.classList.remove("subscription__tabs--active");
 
-        if (i === index) {
-          tab.classList.add('subscription__tabs--active');
-        }
-      });
-    });
-
-    this.controls = document.querySelectorAll(".".concat(controls));
-    this.controlArr = Array.prototype.slice.call(this.controls);
-    this.tabElements = document.querySelectorAll(".".concat(tabElements));
-    this.tabElementArr = Array.prototype.slice.call(this.tabElements);
-    this.initDropdown();
-  }
-
-  _createClass(Tabs, [{
-    key: "initDropdown",
-    value: function initDropdown() {
-      var _this4 = this;
-
-      this.controlArr.forEach(function (el, i, arr) {
-        el.addEventListener('click', function (_ref) {
-          var target = _ref.target;
-
-          _this4.toggleClassBtn(arr, target);
-
-          _this4.toggleTab(_this4.tabElementArr, i);
+          if (i === index) {
+            tab.classList.add("subscription__tabs--active");
+          }
         });
       });
+
+      this.controls = document.querySelectorAll(".".concat(controls));
+      this.controlArr = Array.prototype.slice.call(this.controls);
+      this.tabElements = document.querySelectorAll(".".concat(tabElements));
+      this.tabElementArr = Array.prototype.slice.call(this.tabElements);
+      this.initDropdown();
     }
-  }]);
 
-  return Tabs;
-}();
+    _createClass(Tabs, [{
+      key: "initDropdown",
+      value: function initDropdown() {
+        var _this4 = this;
 
-new Tabs('subscription__tab_control', 'subscription__tabs');
+        this.controlArr.forEach(function (el, i, arr) {
+          el.addEventListener("click", function (_ref) {
+            var target = _ref.target;
+
+            _this4.toggleClassBtn(arr, target);
+
+            _this4.toggleTab(_this4.tabElementArr, i);
+          });
+        });
+      }
+    }]);
+
+    return Tabs;
+  }();
+
+  new Tabs("subscription__tab_control", "subscription__tabs");
+})();
--- source/js/main/slider.js
+++ source/js/main/slider.js
@@ -1,49 +1,53 @@
 'use strict';
+(function() {
 
-class Slider {
-  constructor(slider) {
-    //Управление слайдером
-    this.sliderControl = document.querySelector(`.${slider}__slider_control`);
-    this.sliderBtnNext = this.sliderControl.querySelector(`.${slider}__button_next`);
-    this.sliderBtnBack = this.sliderControl.querySelector(`.${slider}__button_back`);
-    //Слайдер
-    this.sliderBlock = document.querySelector(`.${slider}__slider_block`);
-    this.sliderViewport = this.sliderBlock.offsetWidth;
-    this.sliders = this.sliderBlock.querySelector(`.${slider}__sliders`);
-    this.right = parseInt(window.getComputedStyle(this.sliders, null).getPropertyValue("right"));
-    this.slider = this.sliders.querySelector(`.${slider}__slider_item`);
-    this.sliderMarginRight = parseInt(window.getComputedStyle(this.slider, null).getPropertyValue("margin-right"));
-    this.viewSlide = this.right;
-    this.sliderInit();
-  }
-
-  sliderInit () {
-    this.sliderBtnNext.addEventListener('click', evt => {
-      evt.preventDefault();
-      this.sliderLeft();
-    });
-    this.sliderBtnBack.addEventListener('click', evt => {
-      evt.preventDefault();
-      this.sliderRigth();
-    });
-  }
-
-  sliderLeft = () => {
-    this.viewSlide += (this.sliderViewport + this.sliderMarginRight);
-    if (this.viewSlide > (this.sliders.offsetWidth - (this.sliderViewport + this.sliderMarginRight))) {
-      this.viewSlide = 0;
-    } else {
-      this.viewSlid = this.sliders.offsetWidth - (this.sliderViewport + this.sliderMarginRight);
+  class Slider {
+    constructor(slider) {
+      //Управление слайдером
+      this.sliderControl = document.querySelector(`.${slider}__slider_control`);
+      this.sliderBtnNext = this.sliderControl.querySelector(`.${slider}__button_next`);
+      this.sliderBtnBack = this.sliderControl.querySelector(`.${slider}__button_back`);
+      //Слайдер
+      this.sliderBlock = document.querySelector(`.${slider}__slider_block`);
+      this.sliderViewport = this.sliderBlock.offsetWidth;
+      this.sliders = this.sliderBlock.querySelector(`.${slider}__sliders`);
+      this.right = parseInt(window.getComputedStyle(this.sliders, null).getPropertyValue("right"));
+      this.slider = this.sliders.querySelector(`.${slider}__slider_item`);
+      this.sliderMarginRight = parseInt(window.getComputedStyle(this.slider, null).getPropertyValue("margin-right"));
+      this.viewSlide = this.right;
+      this.sliderInit();
     }
-    this.sliders.style.right = `${this.viewSlide}px`;
+
+    sliderInit () {
+      this.sliderBtnNext.addEventListener(`click`, evt => {
+        evt.preventDefault();
+        this.sliderLeft();
+      });
+      this.sliderBtnBack.addEventListener(`click`, evt => {
+        evt.preventDefault();
+        this.sliderRight();
+      });
+    }
+
+    sliderLeft = () => {
+      this.viewSlide += (this.sliderViewport + this.sliderMarginRight);
+      if (this.viewSlide > (this.sliders.offsetWidth - (this.sliderViewport + this.sliderMarginRight))) {
+        this.viewSlide = 0;
+      } else {
+        this.viewSlid = this.sliders.offsetWidth - (this.sliderViewport + this.sliderMarginRight);
+      }
+      this.sliders.style.right = `${this.viewSlide}px`;
+    }
+
+    sliderRight = () => {
+      this.viewSlide -= (this.sliderViewport + this.sliderMarginRight);
+      this.viewSlide = this.viewSlide < 0 ? (this.sliders.offsetWidth - (this.sliderViewport + this.sliderMarginRight)) : this.viewSlide;
+      this.sliders.style.right = `${this.viewSlide}px`;
+    }
   }
 
-  sliderRigth = () => {
-    this.viewSlide -= (this.sliderViewport + this.sliderMarginRight);
-    this.viewSlide = this.viewSlide < 0 ? (this.sliders.offsetWidth - (this.sliderViewport + this.sliderMarginRight)) : this.viewSlide;
-    this.sliders.style.right = `${this.viewSlide}px`;
-  }
-}
+  new Slider(`coaches`);
+  new Slider(`reviews`);
+})();
 
-new Slider('coaches');
-new Slider('reviews');
+
