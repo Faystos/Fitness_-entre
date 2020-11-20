48186760308aa919a4159596ab63de838cf15a06
Vladimir <fayst_@bk.ru>
2020-11-20 19:52:53 +0300

    Продолжаю тестировать код для определения ios

--- source/js/main/detect_ios.js
+++ source/js/main/detect_ios.js
@@ -1,12 +1,11 @@
 'use strict';
 
-let title = document.querySelector('.description__text p');
+let title = document.querySelector('.description__text h1');
 
 // let detect = navigator.userAgent;
 let detect = !!navigator.platform && /iPad|iPhone|iPod/.test(navigator.platform);
 console.log(detect);
 
 if (detect) {
-  title.style.letterSpacing = 'none';
-  title.textContent = "IOS";
+  title.style.letterSpacing = '0';
 }
